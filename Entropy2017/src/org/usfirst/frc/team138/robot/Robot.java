8c42f866b642177a008a6df8e4ae602a5eae342c
Team138 <Team138@Blue>
2017-01-16 19:35:48 -0500

    Hi

--- Entropy2017/src/org/usfirst/frc/team138/robot/Robot.java
+++ Entropy2017/src/org/usfirst/frc/team138/robot/Robot.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team138.robot.subsystems.*;
-import org.usfirst.frc.team138.robot.commands.*;
+//import org.usfirst.frc.team138.robot.commands.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
