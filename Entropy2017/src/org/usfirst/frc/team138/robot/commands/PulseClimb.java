cdb8173fe8a5b3c014b1215f314c324e7e2f102f
Team138 <Team138@Red>
2017-03-08 20:14:16 -0500

    Post Granite State Competition Push.

---
+++ Entropy2017/src/org/usfirst/frc/team138/robot/commands/PulseClimb.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team138.robot.commands;
+
+import org.usfirst.frc.team138.robot.Robot;
+import org.usfirst.frc.team138.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class PulseClimb extends Command {
+
+	private static final double pulseDuration = 0.5;
+	
+	public PulseClimb()
+	{
+		requires(Robot.climbingMechanism);
+	}
+	
+	protected void execute()
+	{
+		if (Math.floor(timeSinceInitialized() / pulseDuration) % 2 == 0)
+		{
+			Robot.climbingMechanism.setSpeed(RobotMap.ROPE_PULSE_SPEED);
+		}
+		else
+		{
+			Robot.climbingMechanism.setSpeed(0);
+		}
+	}
+	
+	protected boolean isFinished() {
+		return Robot.oi.autoRoutinesCancelled();
+	}
+
+}
